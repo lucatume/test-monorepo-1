59598358b1c81c69e5515e76371d297f3bef525f
Luca Tumedei <luca@theaveragedev.com>
2022-10-21 19:21:17 +0200

    change comment

--- plugin.php
+++ plugin.php
@@ -8,7 +8,7 @@ function test_monorepo_1_activate(){}
 register_activation_hook(__FILE__, 'test_monorepo_1_activate');
 
 function from_monorepo_again(){} // Github
-function from_monorepo_again_2(){}
+function from_monorepo_again_2(){} // Monorepo
 function from_github_1(){}
 function from_github_2(){}
 function from_monorepo_again_3(){}
