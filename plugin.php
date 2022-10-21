e5f971a780ae09936101ca3a9997a26db2702552
Luca Tumedei <luca@theaveragedev.com>
2022-10-21 19:20:50 +0200

    Merge branch 'pll-1' of github.com:lucatume/test-monorepo-1 into pll-1

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@ function test_monorepo_1_activate(){}
 
 register_activation_hook(__FILE__, 'test_monorepo_1_activate');
 
-function from_monorepo_again(){}
+function from_monorepo_again(){} // Github
 function from_monorepo_again_2(){}
 function from_github_1(){}
 function from_github_2(){}
